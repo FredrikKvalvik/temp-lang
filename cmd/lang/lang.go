0b3ac4180f36078df0f3f1d91eabeaa429059219
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2024-09-15 12:00:50 +0200

    chore: add simple entry for interpreter

--- cmd/lang/lang.go
+++ cmd/lang/lang.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Print("hei fra lang")
+	args := os.Args[1:]
+
+	if len(args) > 1 {
+		fmt.Printf("usage: templang [script-path]")
+		os.Exit(1)
+	} else if len(args) == 1 {
+		fmt.Printf("running scripts: %s...", args[0])
+	} else {
+		repl(os.Stdin, os.Stdout)
+	}
+}
+
+func repl(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	fmt.Printf("INSIDE REPL\n")
+	for {
+		fmt.Print("> ")
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Bytes()
+		fmt.Fprintf(out, "%s\n", line)
+	}
 }
